686168653fb3454953cbc0b07273f3f9ee6469e0
Mateusz Kozak <koziu92@live.com>
2016-01-12 21:22:02 +0100

    no message

---
+++ HotelManagementService/ViewModels/RoomsEventsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using HotelManagementService.Models;
+
+namespace HotelManagementService.ViewModels
+{
+  public class RoomsEventsViewModel
+  {
+    public ICollection<RoomModels> Rooms { get; set; }
+    public ICollection<Event> Events { get; set; }  
+  }
+}
